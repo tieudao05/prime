ba0f2a3801a69800795000509f51a6e9c4834d4e
Admin <Admin@LAPTOP-Q9E172C7>
2021-10-24 17:49:40 +0700

    Thực hiện tìm số nguyên tố cùng nhau với N lớn nhất

---
+++ Prime/src/prime/CoPrime.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prime;
+
+/**
+ *
+ * @author ThanhThang
+ */
+public class CoPrime {
+    public int coPrime(int N){
+        return N-1;
+    }
+}
